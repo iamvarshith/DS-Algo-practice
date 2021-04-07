86d585ffd1b924054fc66194809f0dc543fab544
iamvarshith <50661062+iamvarshith@users.noreply.github.com>
2021-04-07 22:44:39 +0530

    binarysearch implementation

--- binarysearch.py
+++ binarysearch.py
@@ -1,12 +1,9 @@
-array = sorted([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+array = sorted([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
 search_element = int(input('enter the number'))
 
 
 def binarysort(start_index: int, end_index: int):
-    print(array)
-    print(start_index, end_index)
     mid = int((start_index + end_index) / 2)
-    print(mid)
 
     if search_element == array[mid]:
         return mid
