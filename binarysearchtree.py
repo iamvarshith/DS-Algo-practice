23deb033ef6dee5820cb4a2e377102cc1e1b55cf
iamvarshith <50661062+iamvarshith@users.noreply.github.com>
2021-04-15 17:42:42 +0530

    BST Implementation of deleting the elements

--- binarysearchtree.py
+++ binarysearchtree.py
@@ -1,39 +1,74 @@
 class Node:
     def __init__(self, data):
         self.data: int = data
-        self.right = None
-        self.left = None
+        self.right: Node = None
+        self.left: Node = None
         self.counter = None
 
 
 def insert(root=None, key=None):
     if root.data is None:
         root = Node(key)
-    if root.data > key:
+
+    elif root.data < key:
         if root.right is None:
             root.right = Node(key)
         if root.right:
             return insert(root=root.right, key=key)
-    if root.data < key:
+    elif root.data > key:
         if root.left is None:
             root.left = Node(key)
         if root.left:
             return insert(root=root.left, key=key)
 
 
-def printTree(r : Node):
-    if r.data:
-        print(r.data, end='/n')
-        if r.left:
-            printTree(r.left)
-        if r.right:
-            printTree(r.right)
-        if r.counter:
-            print('( {} )'.format(r.counter))
+def Inorder(r: Node):
+    if r:
+        Inorder(r.left)
+        print(r.data)
+        Inorder(r.right)
 
-k = Node(5)
-insert(k, 15)
-insert(k, 2)
-insert(k,1)
-printTree(k)
-print(k.data, k.left.data, k.right.data)
+
+def preinorder(r: Node, pre: Node = None):
+    while r.right:
+        r = r.right
+    return (r.data)
+
+
+def deleteelement(r: Node, element):
+    if r.left is None and r.right is None and r.data == element:
+        r.data = None
+        return Inorder(r)
+    elif r.left and r.right is None and r.data == element:
+        r.data = r.left.data
+        r.left = None
+    elif r.right and r.left is None and r.data == element:
+        r.data = r.right.data
+        r.right = None
+
+    elif r.data is None:
+        return print('No items in the tree')
+    elif r.data and r.data != element:
+
+        if element < r.data:
+            return deleteelement(r.left, element)
+        if element > r.data:
+            return deleteelement(r.right, element)
+
+    elif r.right and r.left and r.data == element:
+        k = preinorder(r.left)
+        r.data = k
+        deleteelement(r.left, k)
+
+
+r = Node(20)
+insert(r, 8)
+insert(r, 22)
+insert(r, 24)
+insert(r, 4)
+insert(r, 12)
+insert(r, 10)
+insert(r, 14)
+Inorder(r)
+deleteelement(r, 20)
+Inorder(r)
